42eff25fb82c168725900a4a7be4b770ca0642f1
LonniR <Lonnir2@gmail.com>
2022-08-14 13:49:13 -0400

    edit and update routes complete

--- views/show.jsx
+++ views/show.jsx
@@ -1,22 +1,31 @@
 const React = require('react')
 const Default = require('./layouts/Default')
 
-function Show({ bread }) {
+function Show({ bread, index }) {
     // Confirm we are getting our bread data in the terminal.
     // console.log(bread.name)
     var tomTest = 'Hello from tom JS stuff'
     return (
         <Default>
             <h3>{bread.name}</h3>
+            <a href={`/breads/${index}/edit`}><button>Edit</button></a>
+
+
+            <form action={`/breads/${index}?_method=DELETE`} method="POST">
+                <input type='submit' value="DELETE" />
+            </form>
+
             <p>
                 and it
                 {
                     bread.hasGluten
-                    ? <span> does </span>
-                    : <span> does NOT </span>
+                        ? <span> does </span>
+                        : <span> does NOT </span>
                 }
                 have gluten.
             </p>
+
+
             <img src={bread.image} alt={bread.name} />
             <li><a href="/breads">Go home</a></li>
         </Default>
